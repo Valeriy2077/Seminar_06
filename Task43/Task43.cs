4df7a6562ec9f0201e0ed3dd00c1509f5c1ea049
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-02-21 01:06:47 +0300

    task43

--- Task43/Task43.cs
+++ Task43/Task43.cs
@@ -2,3 +2,16 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.Write("Введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.Write($"Пересечение в точке: ({x};{y})");
