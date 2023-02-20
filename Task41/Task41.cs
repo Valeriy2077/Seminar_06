95e49f8c298e0fa5b1dc7a6cc971293c55e81c50
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-02-21 00:46:01 +0300

    task41

--- Task41/Task41.cs
+++ Task41/Task41.cs
@@ -4,3 +4,16 @@
 
 // 1, -7, 567, 89, 223-> 3
 
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > {count}");
